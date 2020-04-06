0e25e0fc6e8814437e12ed92a303bef85233fc24
Dean Agan <agandfr@gmail.com>
2020-04-06 21:09:09 +1000

    Add test

--- SumNumber/Test/Service/SumNumberServiceTest.cs
+++ SumNumber/Test/Service/SumNumberServiceTest.cs
@@ -16,25 +16,29 @@ namespace Test.Controller
 {
     public class ProductServiceTest
     {
-        private readonly ISumNumberService _sumNumberService;
         private readonly ILogger<SumNumberService> _logger;
         public ProductServiceTest(ITestOutputHelper testOutputHelper)
         {
             var loggerFactory = new LoggerFactory();
             loggerFactory.AddProvider(new XunitLoggerProvider(testOutputHelper));
             _logger = loggerFactory.CreateLogger<SumNumberService>();
-            _sumNumberService = new SumNumberService(_logger);
+
         }
 
         [Theory]
         [InlineData(new object[] {new int[]{2, 7, 11, 15}, 9})]
-        public void AllCodesReturned_WhenGetAllSkuCodesInvoked(List<int> numbers, int target)
+        public void ReturnCorrectPair_WhenFindingSuppliedTarget(int[] numbers, int target)
         {
             // Arrange
+            var sumNumberService = new SumNumberService(_logger);
+            var numberList = new List<int>();
+            numberList.AddRange(numbers);
 
             // Act
+            var result = sumNumberService.GetElementsThatHitTarget(numberList, target);
 
             // Assert
+            result.Should().ContainInOrder(0, 1);
         }
 
 
