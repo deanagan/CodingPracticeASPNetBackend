ee253e5d2f65b875cfabc323fe8f202075709d41
Dean Agan <agandfr@gmail.com>
2020-04-12 17:58:22 +1000

    Implemented skeletal code for vote repo and service

--- VoteCounter/Api/Data/VoteRepository.cs
+++ VoteCounter/Api/Data/VoteRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 using Api.Interfaces;
 using Api.Models;
@@ -9,9 +10,27 @@ namespace Api.Data
     {
         private readonly List<Vote> VotesCounted = new List<Vote>();
 
-        public IList<Vote> GetVotes()
+        public void Reset()
         {
-            return VotesCounted;
+            VotesCounted.Clear();
+        }
+
+        public void AddVoteFor(string name)
+        {
+            AddVoteFor(name, 1);
+        }
+
+        public void AddVoteFor(string name, int votes)
+        {
+            var candidate = VotesCounted.First(vote => vote.Name == name);
+            if (candidate == null)
+            {
+                VotesCounted.Add(new Vote { Name = name, Count = votes});
+            }
+            else
+            {
+                candidate.Count += votes;
+            }
         }
     }
 }
---
+++ VoteCounter/Api/Services/VoteService.cs
@@ -0,0 +1,25 @@
+using Api.Interfaces;
+
+namespace Api.Services
+{
+    public class VoteService : IVoteService
+    {
+        private IVoteRepository _voteRepository;
+        public VoteService(IVoteRepository voteRepository)
+        {
+            _voteRepository = voteRepository;
+        }
+
+        public void AddVoteFor(int id, int votes)
+        {
+            string name = "Donald";
+            _voteRepository.AddVoteFor(name, votes);
+        }
+
+        public void AddVoteFor(int id)
+        {
+            string name ="Donald";
+            _voteRepository.AddVoteFor(name);
+        }
+    }
+}
