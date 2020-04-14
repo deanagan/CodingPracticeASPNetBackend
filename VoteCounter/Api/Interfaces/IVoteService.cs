c174bc4c6d7e5202c4010d5e10b5a3df0cec3380
Dean Agan <agandfr@gmail.com>
2020-04-14 21:12:48 +1000

    Add get winner method to vote service interface

--- VoteCounter/Api/Interfaces/IVoteService.cs
+++ VoteCounter/Api/Interfaces/IVoteService.cs
@@ -2,6 +2,7 @@ namespace Api.Interfaces
 {
     public interface IVoteService
     {
+        string GetWinner();
         void AddVoteFor(int id, int votes);
         void AddVoteFor(int id);
     }
