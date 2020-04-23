c26e444d2eae4cfe894de6577305c9b79386fcb7
Dean Agan <agandfr@gmail.com>
2020-04-23 21:00:25 +1000

    Add vote counter controller test

---
+++ VoteCounter/Test/Controller/VoteCounterControllerTest.cs
@@ -0,0 +1,39 @@
+using Xunit;
+using Xunit.Abstractions;
+using Moq;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+
+using Api.Models;
+using Api.Interfaces;
+using Api.Controllers;
+
+namespace Test.Controller
+{
+    public class VoteCounterControllerTest
+    {
+
+        public VoteCounterControllerTest(ITestOutputHelper testOutputHelper)
+        {
+            var loggerFactory = new LoggerFactory();
+            loggerFactory.AddProvider(new XunitLoggerProvider(testOutputHelper));
+            _logger = loggerFactory.CreateLogger<ProductController>();
+        }
+        [Fact]
+        public void VoteWinnerShouldBeReturned_WhenRequested()
+        {
+            // Arrange
+
+
+            // Act
+
+
+            // Assert
+
+        }
+
+    }
+}
